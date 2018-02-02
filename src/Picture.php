13e118145a7e0f218dabb4dcd8094bfe1e5b4c74
vagrant <vagrant@homestead>
2018-02-02 17:55:04 +0000

    tests passing

--- src/Picture.php
+++ src/Picture.php
@@ -5,89 +5,102 @@ use Illuminate\Support\Facades\Storage;
 
 class Picture
 {
-    /**
-     * Create a new Skeleton Instance
-     */
-    public function __construct()
-    {
-        // constructor body
-    }
+  /**
+   * Create a new Skeleton Instance
+   */
+  public function __construct()
+  {
+      // constructor body
+  }
 
-    public static function size($image)
-    {
-        return list($width, $height, $type, $attr) = getimagesize($image);
-    }
+  public static function size($image)
+  {
+    return list($width, $height, $type, $attr) = getimagesize($image);
+  }
 
-    /**
-     * Creates all directories in the path given
-     * 
-     * @param  String $dirs  Slash separated path: netflix/strangerthings/eleven
-     * @return String        The result of the created path
-     */
-    public static function preparePath($dirs)
-    {
-      $dirString = './';
-      $dirsArray = explode('/', $dirs);
-      foreach ($dirsArray as $dir) {
-        try {
-          $dirString .= $dir . '/';
-          if (!file_exists($dirString)) {
-            mkdir($dirString);
-          }
-        } catch (Exception $e) {
-          dd($e);
+
+  public static function createFromWidth($picture, $width, $name, $path)
+  {
+    $options = [
+      'name' => $name,
+      'path' => $path,
+      'action' => 'resize',
+      'width' => $width,
+      'height' => null,
+      'maintainAspectRatio' => true,
+      'quality' => 90,
+    ];
+    return self::_makePicture($picture, $options);
+  }
+
+  /**
+   * Creates all directories in the path given
+   *
+   * @param  String $dirs  Slash separated path: netflix/strangerthings/eleven
+   * @return String        The result of the created path
+   */
+  public static function preparePath($dirs)
+  {
+    $dirString = './';
+    $dirsArray = explode('/', $dirs);
+    foreach ($dirsArray as $dir) {
+      try {
+        $dirString .= $dir . '/';
+        if (!file_exists($dirString)) {
+          mkdir($dirString);
         }
+      } catch (Exception $e) {
+        dd($e);
       }
-      return $dirString;
     }
+    return $dirString;
+  }
 
+  /**
+   * Método principal da classe, cria uma imagem e transfere
+   * para o local desejado
+   *
+   * @param Array $options
+   */
+  public static function _makePicture($picture, $options = [
+    'name' => 'boooo',
+    'path' => 'uploads',
+    'action' => 'resize',
+    'width' => 300,
+    'height' => null,
+    'maintainAspectRatio' => true,
+    'quality' => 90,
+  ])
+  {
+    $pictureDestinationPath = self::preparePath($options['path']);
+    $extension = $picture->getClientOriginalExtension();
+    $image = Image::make($picture);
 
-    public static function makePictureFromWidth($picture, $width, $newFileName = null, $path = '/', $maintainAspectRatio = false)
-    { 
-      $finalPath = self::preparePath($path);
-      $image = Image::make($picture);
-      $extension = $picture->getClientOriginalExtension();
-      if ($maintainAspectRatio) {
-          $image->resize($width, null, function($constraint) {
-              $constraint->aspectRatio();
+    switch ($options['action']) {
+      case 'crop':
+        # code...
+        break;
+      case 'resize':
+        if ($options['maintainAspectRatio']) {
+          $image->resize($options['width'], null, function($constraint) {
+            $constraint->aspectRatio();
           });
-      } else {
-        $image = Image::make($picture);
-      }
-      $newImageName = !is_null($newFileName) ? $newFileName : time();
-      $newImageName .= $newImageName . '.' . $extension;
-      $finalPath .= DIRECTORY_SEPARATOR . $newImageName;
-      $image->save($finalPath);
-      return $finalPath;
+        } else {
+          $image->resize($options['width'], $options['height']);
+        }
+        break;
+      default:
+        # code...
+        break;
     }
+
+    $finalPath = $pictureDestinationPath .
+                  '/' .
+                  $options['name'] . '.' . $extension;
+    $image->save($finalPath);
+  }
 
 }
 
-      // $extension = $picture->extension();
-      // $newImageName = time() . '.' . $extension;
-
-      // if (!file_exists($basePath)) {
-      //   Storage::makeDirectory($basePath, 0755, true, true);
-      // }
-      // if (!file_exists($basePath . '/uploads')) {
-      //   Storage::makeDirectory($basePath . '/uploads', 0755, true, true);
-      // }
-      // if (!file_exists($path)) {
-      //   Storage::makeDirectory($path, 0755, true, true);
-      // }
-      // if ( !file_exists($pathLarge) ) {
-      //   Storage::makeDirectory($pathLarge, 0755, true, true);
-      // }
-
-      // $image = Image::make($picture);
-      // // $image->crop($cropDimension, $cropDimension);
-      // // $image->save($pathSmall . '/' . $newImageName);
-      // $image->resize($width, null, function($constraint) {
-      //   $constraint->aspectRatio();
-      // });
-      // // $image->resize($width);
-      // // $iamge->encode(100);
-      // $image->save($pathLarge . '/' . $newImageName);
-      // return $pathLarge . '/' . $newImageName;
 
 
--- tests/PictureTest.php
+++ tests/PictureTest.php
@@ -6,10 +6,12 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class PictureTest extends TestCase
 {
-  public $picture = '.\tests\2721.jpg';
-  public $publicPath = '.\tests';
-  public $finalPath = '.\tests' . DIRECTORY_SEPARATOR . 'uploads';
-  public $finalPicturePath = '.\tests' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . 'picture.jpg';
+  public $picture = __DIR__ . '/2721.jpg';
+  public $testPath = __DIR__ .'/uploads/pictures/allofthem/';
+
+  public $publicPath = '.\\';
+  public $finalPath = '.\\' . DIRECTORY_SEPARATOR . 'uploads';
+  public $finalPicturePath = '.\\' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . 'picture.jpg';
   public $newName = 'picture';
   /**
    * Setup the test environment.
@@ -17,6 +19,7 @@ class PictureTest extends TestCase
   protected function setUp()
   {
     parent::setUp();
+    $this->rrmdir('.tests\uploads');
     // rmdir($this->publicPath);
     // rmdir($this->publicPath . '/uploads');
       // Your code here
@@ -39,40 +42,56 @@ class PictureTest extends TestCase
     ];
   }
 
-  // public function test_creates_directories()
-  // {
-  //   $this->assertDirectoryNotExists($this->publicPath, 'O diretório público nao existe');
-  //   Picture::createUploadDir($this->publicPath, '/uploads');
-  //   $this->assertDirectoryExists($this->publicPath, 'Diretorio publico criado');
-  //   rmdir($this->publicPath, true);
-  // }
+  public function rrmdir($dir) {
+   if (is_dir($dir)) {
+     $objects = scandir($dir);
+     foreach ($objects as $object) {
+       if ($object != "." && $object != "..") {
+         if (is_dir($dir."/".$object))
+           rrmdir($dir."/".$object);
+         else
+           unlink($dir."/".$object);
+       }
+     }
+     rmdir($dir);
+   }
+  }
 
-  // public function test_moves_picture()
-  // {
-  //   $picture = new UploadedFile($this->picture, $this->picture);
+  public function makeTestFile()
+  {
+    return new UploadedFile($this->picture, $this->picture);
+  }
 
-  //   $this->assertFileNotExists($this->finalPicturePath, 'The picture is not in its place');
-  //   Picture::makePictureFromWidth($picture, 300, $this->newName, $this->finalPath, true);
-  //   $this->assertFileExists($this->finalPicturePath, 'the picture exists!');
-  // }
-  // 
   public function test_create_whole_path()
   {
     $testPath = 'tests/uploads/netflix/garbage';
     $test = Picture::preparePath($testPath);
-    $this->assertDirectoryExists('tests/uploads', 'Uploads existe');
-    $this->assertDirectoryExists('tests/uploads/netflix', 'netflix existe');
     $this->assertDirectoryExists($testPath, '$testPath ' . $testPath , ' existe');
-    $this->assertContains($test, './' .$testPath, 'test ' . $test . ' e ' . $testPath);
-    $this->delTree('./tests/uploads');
+    $this->assertEquals('./tests/uploads/netflix/garbage/', $test);
+    // $this->delTree('./tests/uploads');
+  }
+
+  public function test_make_picture()
+  {
+    $options = [
+      'name' => 'picture',
+      'path' => $this->testPath,
+      'action' => 'resize',
+      'width' => 300,
+      'height' => null,
+      'maintainAspectRatio' => true,
+      'quality' => 90,
+    ];
+    Picture::_makePicture($this->makeTestFile(), $options);
+    $this->assertFileExists('uploads/picture/long/path/picture.jpg');
   }
 
 
-  public function delTree($dir) { 
-   $files = array_diff(scandir($dir), array('.','..'));
-    foreach ($files as $file) { 
-      (is_dir("$dir/$file")) ? $this->delTree("$dir/$file") : unlink("$dir/$file"); 
-    } 
-    return rmdir($dir); 
-  } 
+  // public function delTree($dir) {
+  //  $files = array_diff(scandir($dir), array('.','..'));
+  //   foreach ($files as $file) {
+  //     (is_dir("$dir/$file")) ? $this->delTree("$dir/$file") : unlink("$dir/$file");
+  //   }
+  //   return rmdir($dir);
+  // }
 }
